bde53c09f7fc09b5141163dbf1848a3f14c24ecc
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-07 22:55:12 +0200

    Add winup.ps1 draft

---
+++ winfiles/win_up.ps1
@@ -0,0 +1,62 @@
+<#
+1. Download and install updates
+2. Download and install all drivers + printer
+3. Create a restore point
+
+4. Restart PC
+5. Download and use CTT - winutil
+6. Create a restore point
+
+7. Restart PC
+8. Install utility applications:
+  1. Microsoft.VCRedist.2005.x64
+  2. Microsoft.VCRedist.2008.x64
+  3. Microsoft.VCRedist.2010.x64
+  4. Microsoft.VCRedist.2012.x64
+  5. Microsoft.VCRedist.2013.x64
+  6. Microsoft.VCRedist.2015+.x64
+  7. File-New-Project.EarTrumpet
+  8. 7zip.7zip
+  9. CPUID.HWMonitor
+  10. Notepad++.Notepad++
+  11. SumatraPDF.SumatraPDF
+  12. Brave.Brave
+  13. KeePassXCTeam.KeePassXC
+  14. Greenshot.Greenshot
+  15. VideoLAN.VLC
+  16. TheDocumentFoundation.LibreOffice
+  17. Microsoft.PowerToys
+
+9. Restart PC
+10. Install work applications:
+  1. Git.Git
+  2. Microsoft.PowerShell
+  3. Microsoft.AzureCLI
+  4. Microsoft.WindowsTerminal
+  5. Microsoft.VisualStudioCode
+
+11. Download arkade - https://github.com/alexellis/arkade
+
+12. Restart PC
+13. Install Powershell modules:
+  1. PSWindowsUpdate
+  2. PSReadLine
+  3. z
+  4. PSColor
+  5. Terminal-Icons
+  6. posh-git
+  7. Az
+
+14. Setup powershell profile
+
+15. Restart PC
+16. Install WSL
+17. Setup WSL config
+
+18. Restart PC
+19. Setup Windows Terminal
+#>
+
+<#
+
+#>
