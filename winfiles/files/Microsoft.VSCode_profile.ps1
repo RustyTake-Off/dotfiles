ab6ae6389bec7e463d500ee945ffcbd26cfa0424
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-11 21:41:15 +0200

    Add Microsoft.VSCode_profile.ps1 settings

---
+++ winfiles/files/Microsoft.VSCode_profile.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+Configuration for the Microsoft.VSCode_profile.ps1.
+
+.DESCRIPTION
+Configuration for the Microsoft.VSCode_profile.ps1.
+Loads the Microsoft.PowerShell_profile.ps1.
+#>
+. %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
