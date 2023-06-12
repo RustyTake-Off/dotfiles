abf21cffe997d909ee7a1d82b28f61212f48c515
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-13 00:37:23 +0200

    Update Microsoft.PowerShell_profile.ps1 config

--- winfiles/files/Microsoft.PowerShell_profile.ps1
+++ winfiles/files/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,28 @@
 Configuration for the Microsoft.PowerShell_profile.ps1.
 #>
 
+# From https://serverfault.com/questions/95431/in-a-powershell-script-how-can-i-check-if-im-running-with-administrator-privil#97599
+function Test-Administrator {
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+	(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
 # Import modules
 if ($host.Name -eq 'ConsoleHost') {
   Import-Module -Name PSReadLine
 }
 Import-Module -Name Terminal-Icons
 Import-Module -Name z
+Import-Module -Name PSColors
+Import-Module -Name PSWindowsUpdate
+
+# Tab completion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+  $Local:word = $wordToComplete.Replace('"', '""')
+  $Local:ast = $commandAst.ToString().Replace('"', '""')
+  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
