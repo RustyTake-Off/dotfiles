ffd4665576145b3fb71d6cd3a3caabea4fac58f5
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-14 03:13:03 +0200

    Update Microsoft.PowerShell_profile.ps1 config file

--- winfiles/files/Microsoft.PowerShell_profile.ps1
+++ winfiles/files/Microsoft.PowerShell_profile.ps1
@@ -16,7 +16,6 @@ if ($host.Name -eq 'ConsoleHost') {
 Import-Module -Name Terminal-Icons
 Import-Module -Name z
 Import-Module -Name PSColors
-Import-Module -Name PSWindowsUpdate
 
 # Tab completion
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
@@ -27,4 +26,21 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
   winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
     [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
   }
+}
+
+Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+  param($commandName, $wordToComplete, $cursorPosition)
+  $completion_file = New-TemporaryFile
+  $env:ARGCOMPLETE_USE_TEMPFILES = 1
+  $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+  $env:COMP_LINE = $wordToComplete
+  $env:COMP_POINT = $cursorPosition
+  $env:_ARGCOMPLETE = 1
+  $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+  $env:_ARGCOMPLETE_IFS = "`n"
+  az 2>&1 | Out-Null
+  Get-Content $completion_file | Sort-Object | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+  }
+  Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS
 }
