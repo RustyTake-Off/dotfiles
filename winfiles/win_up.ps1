de0c09ccb1c4913475a94e6e977b35bf21240aa9
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-14 17:53:48 +0200

    Update winup.ps1

--- winfiles/win_up.ps1
+++ winfiles/win_up.ps1
@@ -2,69 +2,75 @@ param (
   [Parameter(Mandatory = $false, Position = 0)]
   [string]$action
 )
-# 01. Download and install updates
-# 02. Download and install all drivers + printer
+
 function Get-Drivers {
   Write-Host "`nDownloading drivers + printer drivers..." -ForegroundColor Yellow
 
   $downloadsPath = Join-Path $Env:USERPROFILE "Downloads"
-  $driversPath = Join-Path $downloadsPath "drivers"
+  $jsonPath = Join-Path $downloadsPath "winup"
 
-  if (-not (Test-Path -Path $driversPath -PathType Container)) {
-    Write-Host "Creating 'drivers' folder..." -ForegroundColor Yellow
-    New-Item -Path $driversPath -ItemType Directory | Out-Null
+  if (-not (Test-Path -Path $jsonPath -PathType Container)) {
+    Write-Host "Creating 'winup' folder..." -ForegroundColor Yellow
+    New-Item -Path $jsonPath -ItemType Directory | Out-Null
   }
 
   $driversUrl = "https://raw.githubusercontent.com/RustyTake-Off/dotfiles/main/winfiles/files/drivers.json"
-  $driversJsonPath = Join-Path $driversPath "drivers.json"
+  $driversJsonPath = Join-Path $jsonPath "drivers.json"
 
   Write-Host "Downloading drivers.json file..." -ForegroundColor Yellow
-  Start-BitsTransfer -Source $driversUrl -Destination $driversPath
+  Start-BitsTransfer -Source $driversUrl -Destination $jsonPath
 
   $driversJson = Get-Content -Raw -Path $driversJsonPath | ConvertFrom-Json
 
   foreach ($driverUrl in $driversJson.drivers) {
-    Write-Host "Downloading: $driverUrl"
+    Write-Host "Downloading " -NoNewline; Write-Host "$driverUrl" -ForegroundColor Blue
     $outputFileName = Split-Path -Leaf $driverUrl
     $outputFilePath = Join-Path $driversPath $outputFileName
     Start-BitsTransfer -Source $driverUrl -Destination $outputFilePath
   }
 
-  Write-Host "Download completed!" -ForegroundColor Green
+  Write-Host "Download of drivers completed!" -ForegroundColor Green
 }
 
-# 03. Invoke CTT - winutil
-function Get-CTT {
-  Write-Host "`nInvoking CTT - winutil" -ForegroundColor Yellow
+function Invoke-CTT {
+  Write-Host "`nInvoking CTT - winutil..." -ForegroundColor Yellow
 
   Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression
+
+  Write-Host "Invoke of winutil completed!" -ForegroundColor Green
 }
 
-# 04. Install utility applications:
-#   01. Microsoft.VCRedist.2005.x64
-#   02. Microsoft.VCRedist.2008.x64
-#   03. Microsoft.VCRedist.2010.x64
-#   04. Microsoft.VCRedist.2012.x64
-#   05. Microsoft.VCRedist.2013.x64
-#   06. Microsoft.VCRedist.2015+.x64
-#   07. File-New-Project.EarTrumpet
-#   08. 7zip.7zip
-#   09. CPUID.HWMonitor
-#   10. Notepad++.Notepad++
-#   11. SumatraPDF.SumatraPDF
-#   12. Brave.Brave
-#   13. KeePassXCTeam.KeePassXC
-#   14. Greenshot.Greenshot
-#   15. VideoLAN.VLC
-#   16. TheDocumentFoundation.LibreOffice
-#   17. Microsoft.PowerToys
+function Get-Apps {
+  Write-Host "`nInstalling applications..." -ForegroundColor Yellow
 
-# 05. Install work applications:
-#   01. Git.Git
-#   02. Microsoft.PowerShell
-#   03. Microsoft.AzureCLI
-#   04. Microsoft.WindowsTerminal
-#   05. Microsoft.VisualStudioCode
+  $downloadsPath = Join-Path $Env:USERPROFILE "Downloads"
+  $jsonPath = Join-Path $downloadsPath "winup"
+
+  if (-not (Test-Path -Path $jsonPath -PathType Container)) {
+    Write-Host "Creating 'winup' folder..." -ForegroundColor Yellow
+    New-Item -Path $jsonPath -ItemType Directory | Out-Null
+  }
+
+  $appsUrl = "https://raw.githubusercontent.com/RustyTake-Off/dotfiles/main/winfiles/files/apps.json"
+  $appsJsonPath = Join-Path $jsonPath "apps.json"
+
+  Write-Host "Downloading apps.json file..." -ForegroundColor Yellow
+  Start-BitsTransfer -Source $appsUrl -Destination $jsonPath
+
+  $appsJson = Get-Content -Raw -Path $appsJsonPath | ConvertFrom-Json
+
+  foreach ($appName in $appsJson.apps) {
+    if (-not (winget list --exact --id $appName)) {
+      Write-Host "Installing " -NoNewline; Write-Host "$appName" -ForegroundColor Blue
+      winget install --exact --id $appName --silent --no-upgrade
+    }
+    else {
+      Write-Host "$appName " -ForegroundColor Blue -NoNewline; Write-Host "is already installed. " -NoNewline; Write-Host "Skipping installation..." -ForegroundColor Red
+    }
+  }
+
+  Write-Host "Installation of applications completed!" -ForegroundColor Green
+}
 
 # 06. Download arkade - https://github.com/alexellis/arkade
 
@@ -78,10 +84,54 @@ function Get-CTT {
 #   07. Az
 #   08. CompletionPredictor
 
-# 08. Setup powershell profile
+function Get-PSModules {
+  Write-Host "`nInstalling PowerShell modules..." -ForegroundColor Yellow
 
-# 09. Install WSL
-# 10. Setup WSL config
+  $downloadsPath = Join-Path $Env:USERPROFILE "Downloads"
+  $jsonPath = Join-Path $downloadsPath "winup"
+
+  if (-not (Test-Path -Path $jsonPath -PathType Container)) {
+    Write-Host "Creating 'winup' folder..." -ForegroundColor Yellow
+    New-Item -Path $jsonPath -ItemType Directory | Out-Null
+  }
+
+  $psmodulesUrl = "https://raw.githubusercontent.com/RustyTake-Off/dotfiles/main/winfiles/files/psmodules.json"
+  $psmodulesJsonPath = Join-Path $jsonPath "psmodules.json"
+
+  Write-Host "Downloading psmodules.json file..." -ForegroundColor Yellow
+  Start-BitsTransfer -Source $psmodulesUrl -Destination $jsonPath
+
+  $psmodulesJson = Get-Content -Raw -Path $psmodulesJsonPath | ConvertFrom-Json
+
+  foreach ($psmName in $psmodulesJson.psmodules) {
+    if (-not (Get-Module -ListAvailable -Name $psmName)) {
+      Write-Host "Installing " -NoNewline; Write-Host "$psmName" -ForegroundColor Blue
+      Install-Module $psmName -Force -AcceptLicense
+    }
+    else {
+      Write-Host "$psmName " -ForegroundColor Blue -NoNewline; Write-Host "is already installed. " -NoNewline; Write-Host "Skipping installation..." -ForegroundColor Red
+    }
+  }
+
+  Write-Host "Installation of PowerShell modules completed!" -ForegroundColor Green
+}
+
+
+function Update-Configs {
+
+  $wingetUrl = "https://raw.githubusercontent.com/RustyTake-Off/dotfiles/main/winfiles/files/winget.json"
+  $wingetConfigPath = Join-Path $Env:LOCALAPPDATA "\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState"
+  $wingetSettingsPath = Join-Path $wingetConfigPath "settings.json"
+
+  Write-Host "Downloading winget.json file..." -ForegroundColor Yellow
+  Start-BitsTransfer -Source $wingetUrl -Destination $wingetSettingsPath
+  Write-Host "Copying winget settings.json file..." -ForegroundColor Yellow
+
+
+
+
+}
+# 08. Setup powershell profile
 
 # 12. Setup Windows Terminal
 
@@ -90,14 +140,25 @@ switch ($action) {
     Get-Drivers
   }
   "ctt" {
-    Get-CTT
+    Invoke-CTT
+  }
+  "apps" {
+    Get-Apps
+  }
+  "psmods" {
+    Get-PSModules
+  }
+  "configs" {
+    Update-Configs
   }
   default {
-    Write-Host """
-    Available actions to take:
-      -drivers
-      -ctt
-
-    """
+    Write-Host "
+    `rAvailable actions to take:
+    drivers     - Downloads drivers
+    ctt         - Invokes the CTT - winutil
+    apps        - Installs applications
+    psmods      - Installs PowerShell modules
+    configs     - Download configuration and settings files
+    "
   }
 }
