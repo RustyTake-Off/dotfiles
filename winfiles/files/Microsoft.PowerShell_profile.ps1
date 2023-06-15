85d398807d7f3b52384dde0da8d0ba2936f13313
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-15 18:18:32 +0200

    Update Microsoft.PowerShell_profile.ps1

--- winfiles/files/Microsoft.PowerShell_profile.ps1
+++ winfiles/files/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,17 @@
 <#
+
 .SYNOPSIS
 Configuration for the Microsoft.PowerShell_profile.ps1.
+
+.DESCRIPTION
+Configuration for the Microsoft.PowerShell_profile.ps1.
+This file should be stored in $PROFILE.CurrentUserAllHosts
+If $PROFILE.CurrentUserAllHosts doesn't exist, you can make one with the following:
+
+PS> New-Item $PROFILE.CurrentUserAllHosts -ItemType File -Force
+
+This will create the file and the containing subdirectory if it doesn't already
+
 #>
 
 # From https://serverfault.com/questions/95431/in-a-powershell-script-how-can-i-check-if-im-running-with-administrator-privil#97599
@@ -10,14 +21,267 @@ function Test-Administrator {
 }
 
 # Import modules
-if ($host.Name -eq 'ConsoleHost') {
-  Import-Module -Name PSReadLine
-}
+# if ($host.Name -eq 'ConsoleHost') {
+#   Import-Module -Name PSReadLine
+# }
+# Import-Module -Name PSColors
 Import-Module -Name Terminal-Icons
+Import-Module -Name posh-git
 Import-Module -Name z
-Import-Module -Name PSColors
+
+# PSReadLine configuration
+$psMinimumVersion = [version]'7.1.999'
+
+if (($Host.Name -eq 'ConsoleHost') -and ($PSVersionTable.PSVersion -ge $psMinimumVersion)) {
+  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+}
+else {
+  Set-PSReadLineOption -PredictionSource History
+}
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineKeyHandler -Key '(', '{', '[' `
+  -BriefDescription InsertPairedBraces `
+  -LongDescription "Insert matching braces" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $closeChar = switch ($key.KeyChar) {
+    <#case#> '(' {
+      [char]')'; break
+    }
+    <#case#> '{' {
+      [char]'}'; break
+    }
+    <#case#> '[' {
+      [char]']'; break
+    }
+  }
+
+  $selectionStart = $null
+  $selectionLength = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+  if ($selectionStart -ne -1) {
+    # Text is selected, wrap it in brackets
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $key.KeyChar + $line.SubString($selectionStart, $selectionLength) + $closeChar)
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+  }
+  else {
+    # No text is selected, insert a pair
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)$closeChar")
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+  }
+}
+
+Set-PSReadLineKeyHandler -Key ')', ']', '}' `
+  -BriefDescription SmartCloseBraces `
+  -LongDescription "Insert closing brace or skip" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+  if ($line[$cursor] -eq $key.KeyChar) {
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+  }
+  else {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
+  }
+}
+
+Set-PSReadLineKeyHandler -Key Backspace `
+  -BriefDescription SmartBackspace `
+  -LongDescription "Delete previous character or matching quotes/parens/braces" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+  if ($cursor -gt 0) {
+    $toMatch = $null
+    if ($cursor -lt $line.Length) {
+      switch ($line[$cursor]) {
+        <#case#> '"' {
+          $toMatch = '"'; break
+        }
+        <#case#> "'" {
+          $toMatch = "'"; break
+        }
+        <#case#> ')' {
+          $toMatch = '('; break
+        }
+        <#case#> ']' {
+          $toMatch = '['; break
+        }
+        <#case#> '}' {
+          $toMatch = '{'; break
+        }
+      }
+    }
+
+    if ($toMatch -ne $null -and $line[$cursor - 1] -eq $toMatch) {
+      [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor - 1, 2)
+    }
+    else {
+      [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteChar($key, $arg)
+    }
+  }
+}
+
+Set-PSReadLineKeyHandler -Key Alt+w `
+  -BriefDescription SaveInHistory `
+  -LongDescription "Save current line in history but do not execute" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+  [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
+  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+}
+
+Set-PSReadLineKeyHandler -Key 'Alt+(' `
+  -BriefDescription ParenthesizeSelection `
+  -LongDescription "Put parenthesis around the selection or entire line and move the cursor to after the closing parenthesis" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $selectionStart = $null
+  $selectionLength = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+  if ($selectionStart -ne -1) {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, '(' + $line.SubString($selectionStart, $selectionLength) + ')')
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+  }
+  else {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, $line.Length, '(' + $line + ')')
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+  }
+}
+
+Set-PSReadLineKeyHandler -Key "Alt+'" `
+  -BriefDescription ToggleQuoteArgument `
+  -LongDescription "Toggle quotes on the argument under the cursor" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $ast = $null
+  $tokens = $null
+  $errors = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+
+  $tokenToChange = $null
+  foreach ($token in $tokens) {
+    $extent = $token.Extent
+    if ($extent.StartOffset -le $cursor -and $extent.EndOffset -ge $cursor) {
+      $tokenToChange = $token
+
+      # If the cursor is at the end (it's really 1 past the end) of the previous token,
+      # we only want to change the previous token if there is no token under the cursor
+      if ($extent.EndOffset -eq $cursor -and $foreach.MoveNext()) {
+        $nextToken = $foreach.Current
+        if ($nextToken.Extent.StartOffset -eq $cursor) {
+          $tokenToChange = $nextToken
+        }
+      }
+      break
+    }
+  }
+
+  if ($tokenToChange -ne $null) {
+    $extent = $tokenToChange.Extent
+    $tokenText = $extent.Text
+    if ($tokenText[0] -eq '"' -and $tokenText[-1] -eq '"') {
+      # Switch to no quotes
+      $replacement = $tokenText.Substring(1, $tokenText.Length - 2)
+    }
+    elseif ($tokenText[0] -eq "'" -and $tokenText[-1] -eq "'") {
+      # Switch to double quotes
+      $replacement = '"' + $tokenText.Substring(1, $tokenText.Length - 2) + '"'
+    }
+    else {
+      # Add single quotes
+      $replacement = "'" + $tokenText + "'"
+    }
+
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+      $extent.StartOffset,
+      $tokenText.Length,
+      $replacement)
+  }
+}
+
+Set-PSReadLineKeyHandler -Key F1 `
+  -BriefDescription CommandHelp `
+  -LongDescription "Open the help window for the current command" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $ast = $null
+  $tokens = $null
+  $errors = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+
+  $commandAst = $ast.FindAll( {
+      $node = $args[0]
+      $node -is [CommandAst] -and
+      $node.Extent.StartOffset -le $cursor -and
+      $node.Extent.EndOffset -ge $cursor
+    }, $true) | Select-Object -Last 1
+
+  if ($commandAst -ne $null) {
+    $commandName = $commandAst.GetCommandName()
+    if ($commandName -ne $null) {
+      $command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
+      if ($command -is [AliasInfo]) {
+        $commandName = $command.ResolvedCommandName
+      }
+
+      if ($commandName -ne $null) {
+        Get-Help $commandName -ShowWindow
+      }
+    }
+  }
+}
+
+# Useful functions
+# Traversing files and folders
+function . { Set-Location .. }
+function .. { Set-Location ..\.. }
+function ... { Set-Location ..\..\.. }
+function .... { Set-Location ..\..\..\.. }
+function ..... { Set-Location ..\..\..\..\.. }
+function hm { Set-Location $env:USERPROFILE }
+function hpr { Set-Location $env:USERPROFILE\pr }
+function hwk { Set-Location $env:USERPROFILE\wk }
+
+# Compute file hashes - useful for checking successful downloads
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
 # Tab completion
+# Tab completion for winget https://learn.microsoft.com/en-us/windows/package-manager/winget/tab-completion
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
   param($wordToComplete, $commandAst, $cursorPosition)
   [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
@@ -28,6 +292,7 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
   }
 }
 
+# Tab completion for azcli https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?view=azure-cli-latest&tabs=winget#enable-tab-completion-on-powershell
 Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
   param($commandName, $wordToComplete, $cursorPosition)
   $completion_file = New-TemporaryFile
