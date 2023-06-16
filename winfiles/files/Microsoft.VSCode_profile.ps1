199feef08861fd5edd58e61ab8a09a0961b04ed8
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-17 01:18:55 +0200

    Update Microsoft.VSCode_profile.ps1

--- winfiles/files/Microsoft.VSCode_profile.ps1
+++ winfiles/files/Microsoft.VSCode_profile.ps1
@@ -1,9 +1,12 @@
 <#
+
 .SYNOPSIS
 Configuration for the Microsoft.VSCode_profile.ps1.
 
 .DESCRIPTION
 Configuration for the Microsoft.VSCode_profile.ps1.
 Loads the Microsoft.PowerShell_profile.ps1.
+
 #>
+
 . %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
