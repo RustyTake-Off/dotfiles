cadf460abab089308a4cdc1e55bc910964002a88
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-17 21:44:25 +0200

    Update Microsoft.PowerShell_profile.ps1

--- winfiles/files/Microsoft.PowerShell_profile.ps1
+++ winfiles/files/Microsoft.PowerShell_profile.ps1
@@ -282,41 +282,6 @@ Set-PSReadLineKeyHandler -Key "Alt+'" `
   }
 }
 
-# F1 for help on the command line - naturally
-Set-PSReadLineKeyHandler -Key F1 `
-  -BriefDescription CommandHelp `
-  -LongDescription "Open the help window for the current command" `
-  -ScriptBlock {
-  param($key, $arg)
-
-  $ast = $null
-  $tokens = $null
-  $errors = $null
-  $cursor = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
-
-  $commandAst = $ast.FindAll( {
-      $node = $args[0]
-      $node -is [CommandAst] -and
-      $node.Extent.StartOffset -le $cursor -and
-      $node.Extent.EndOffset -ge $cursor
-    }, $true) | Select-Object -Last 1
-
-  if ($commandAst -ne $null) {
-    $commandName = $commandAst.GetCommandName()
-    if ($commandName -ne $null) {
-      $command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
-      if ($command -is [AliasInfo]) {
-        $commandName = $command.ResolvedCommandName
-      }
-
-      if ($commandName -ne $null) {
-        Get-Help $commandName -ShowWindow
-      }
-    }
-  }
-}
-
 # This key handler shows the entire or filtered history using Out-GridView. The
 # typed text is used as the substring pattern for filtering. A selected command
 # is inserted to the command line without invoking. Multiple command selection
