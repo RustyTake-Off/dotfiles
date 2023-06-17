4de16e535c1ac4b42f5358f1667f5d86de614e36
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-06-17 15:53:16 +0200

    Update Microsoft.PowerShell_profile.ps1

--- winfiles/files/Microsoft.PowerShell_profile.ps1
+++ winfiles/files/Microsoft.PowerShell_profile.ps1
@@ -50,11 +50,11 @@ Import-Module -Name z
 
 # Useful functions
 # Traversing files and folders
-function . { Set-Location .. }
-function .. { Set-Location ..\.. }
-function ... { Set-Location ..\..\.. }
-function .... { Set-Location ..\..\..\.. }
-function ..... { Set-Location ..\..\..\..\.. }
+function cd. { Set-Location .. }
+function cd.. { Set-Location ..\.. }
+function cd... { Set-Location ..\..\.. }
+function cd.... { Set-Location ..\..\..\.. }
+function cd..... { Set-Location ..\..\..\..\.. }
 function hm { Set-Location $env:USERPROFILE }
 function hpr { Set-Location $env:USERPROFILE\pr }
 function hwk { Set-Location $env:USERPROFILE\wk }
@@ -63,6 +63,34 @@ function hwk { Set-Location $env:USERPROFILE\wk }
 function md5 { Get-FileHash -Algorithm MD5 $args }
 function sha1 { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+function unzip ($file) {
+  Write-Output("Extracting", $file, "to", $pwd)
+  $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+  Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+function touch ($file) {
+  "" | Out-File $file -Encoding ASCII
+}
+
+function which ($name) {
+  Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+# Make it easier to edit this profile once it's installed
+function Edit-Profile {
+  if ($host.Name -match "ise") {
+    $psISE.CurrentPowerShellTab.Files.Add($PROFILE)
+  }
+  else {
+    notepad $PROFILE
+  }
+}
+
+function Reset-Profile {
+  & $PROFILE
+}
 
 # PSReadLine configuration
 $psMinimumVersion = [version]'7.1.999'
@@ -81,105 +109,6 @@ Set-PSReadLineOption -Colors @{ InlinePrediction = 'Blue' }
 
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-# The next four key handlers are designed to make entering matched quotes
-# parens, and braces a nicer experience. I'd like to include functions
-# in the module that do this, but this implementation still isn't as smart
-# as ReSharper, so I'm just providing it as a sample.
-<#Set-PSReadLineKeyHandler -Key '"', "'" `
-  -BriefDescription SmartInsertQuote `
-  -LongDescription "Insert paired quotes if not already on a quote" `
-  -ScriptBlock {
-  param($key, $arg)
-
-  $quote = $key.KeyChar
-
-  $selectionStart = $null
-  $selectionLength = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-
-  $line = $null
-  $cursor = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-  # If text is selected, just quote it without any smarts
-  if ($selectionStart -ne -1) {
-    [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $quote + $line.SubString($selectionStart, $selectionLength) + $quote)
-    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-    return
-  }
-
-  $ast = $null
-  $tokens = $null
-  $parseErrors = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
-
-  function FindToken {
-    param($tokens, $cursor)
-
-    foreach ($token in $tokens) {
-      if ($cursor -lt $token.Extent.StartOffset) { continue }
-      if ($cursor -lt $token.Extent.EndOffset) {
-        $result = $token
-        $token = $token -as [StringExpandableToken]
-        if ($token) {
-          $nested = FindToken $token.NestedTokens $cursor
-          if ($nested) { $result = $nested }
-        }
-
-        return $result
-      }
-    }
-    return $null
-  }
-
-  $token = FindToken $tokens $cursor
-
-  # If we're on or inside a **quoted** string token (so not generic), we need to be smarter
-  if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
-    # If we're at the start of the string, assume we're inserting a new string
-    if ($token.Extent.StartOffset -eq $cursor) {
-      [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote ")
-      [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-      return
-    }
-
-    # If we're at the end of the string, move over the closing quote if present.
-    if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
-      [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-      return
-    }
-  }
-
-  if ($null -eq $token -or
-    $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
-    if ($line[0..$cursor].Where{ $_ -eq $quote }.Count % 2 -eq 1) {
-      # Odd number of quotes before the cursor, insert a single quote
-      [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
-    }
-    else {
-      # Insert matching quotes, move cursor to be in between the quotes
-      [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote")
-      [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-    }
-    return
-  }
-
-  # If cursor is at the start of a token, enclose it in quotes.
-  if ($token.Extent.StartOffset -eq $cursor) {
-    if ($token.Kind -eq [TokenKind]::Generic -or $token.Kind -eq [TokenKind]::Identifier -or
-      $token.Kind -eq [TokenKind]::Variable -or $token.TokenFlags.hasFlag([TokenFlags]::Keyword)) {
-      $end = $token.Extent.EndOffset
-      $len = $end - $cursor
-      [Microsoft.PowerShell.PSConsoleReadLine]::Replace($cursor, $len, $quote + $line.SubString($cursor, $len) + $quote)
-      [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($end + 2)
-      return
-    }
-  }
-
-  # We failed to be smart, so just insert a single quote
-  [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
-}#>
 
 Set-PSReadLineKeyHandler -Key '(', '{', '[' `
   -BriefDescription InsertPairedBraces `
@@ -297,32 +226,6 @@ Set-PSReadLineKeyHandler -Key Ctrl+V `
   }
 }
 
-# Sometimes you want to get a property of invoke a member on what you've entered so far
-# but you need parens to do that. This binding will help by putting parens around the current selection,
-# or if nothing is selected, the whole line.
-<#Set-PSReadLineKeyHandler -Key 'Alt+(' `
-  -BriefDescription ParenthesizeSelection `
-  -LongDescription "Put parenthesis around the selection or entire line and move the cursor to after the closing parenthesis" `
-  -ScriptBlock {
-  param($key, $arg)
-
-  $selectionStart = $null
-  $selectionLength = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-
-  $line = $null
-  $cursor = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-  if ($selectionStart -ne -1) {
-    [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, '(' + $line.SubString($selectionStart, $selectionLength) + ')')
-    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-  }
-  else {
-    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, $line.Length, '(' + $line + ')')
-    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
-  }
-}#>
-
 # Each time you press Alt+', this key handler will change the token
 # under or before the cursor. It will cycle through single quotes, double quotes, or
 # no quotes each time it is invoked.
@@ -379,44 +282,8 @@ Set-PSReadLineKeyHandler -Key "Alt+'" `
   }
 }
 
-# This example will replace any aliases on the command line with the resolved commands.
-<#Set-PSReadLineKeyHandler -Key "Alt+%" `
-  -BriefDescription ExpandAliases `
-  -LongDescription "Replace all aliases with the full command" `
-  -ScriptBlock {
-  param($key, $arg)
-
-  $ast = $null
-  $tokens = $null
-  $errors = $null
-  $cursor = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
-
-  $startAdjustment = 0
-  foreach ($token in $tokens) {
-    if ($token.TokenFlags -band [TokenFlags]::CommandName) {
-      $alias = $ExecutionContext.InvokeCommand.GetCommand($token.Extent.Text, 'Alias')
-      if ($alias -ne $null) {
-        $resolvedCommand = $alias.ResolvedCommandName
-        if ($resolvedCommand -ne $null) {
-          $extent = $token.Extent
-          $length = $extent.EndOffset - $extent.StartOffset
-          [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
-            $extent.StartOffset + $startAdjustment,
-            $length,
-            $resolvedCommand)
-
-          # Our copy of the tokens won't have been updated, so we need to
-          # adjust by the difference in length
-          $startAdjustment += ($resolvedCommand.Length - $length)
-        }
-      }
-    }
-  }
-}#>
-
 # F1 for help on the command line - naturally
-<#Set-PSReadLineKeyHandler -Key F1 `
+Set-PSReadLineKeyHandler -Key F1 `
   -BriefDescription CommandHelp `
   -LongDescription "Open the help window for the current command" `
   -ScriptBlock {
@@ -448,7 +315,7 @@ Set-PSReadLineKeyHandler -Key "Alt+'" `
       }
     }
   }
-}#>
+}
 
 # This key handler shows the entire or filtered history using Out-GridView. The
 # typed text is used as the substring pattern for filtering. A selected command
