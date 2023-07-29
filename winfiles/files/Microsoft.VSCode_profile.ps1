453ec236e23f6d1588e1c3371587dd58cc7cfab0
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-07-29 23:43:43 +0200

    Simple update :up:

--- winfiles/files/Microsoft.VSCode_profile.ps1
+++ winfiles/files/Microsoft.VSCode_profile.ps1
@@ -1,15 +1,15 @@
 <#
 
 .NOTES
-    Author: RustyTake-Off
-    GitHub: https://github.com/RustyTake-Off/dotfiles/tree/main/winfiles
+  Author: RustyTake-Off
+  GitHub: https://github.com/RustyTake-Off/dotfiles/tree/main/winfiles
 
 .SYNOPSIS
-    Configuration for the Microsoft.VSCode_profile.ps1.
+  Configuration for the Microsoft.VSCode_profile.ps1.
 
 .DESCRIPTION
-    Configuration for the Microsoft.VSCode_profile.ps1.
-    Loads the Microsoft.PowerShell_profile.ps1.
+  Configuration for the Microsoft.VSCode_profile.ps1.
+  Loads the Microsoft.PowerShell_profile.ps1.
 
 #>
 
