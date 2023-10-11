50b66a163659f709d81d6da719e5a8018de03bdd
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-10-11 18:21:37 +0200

    Update :large_blue_circle: Powershell profile (and VSCode profile)

--- winfiles/files/Microsoft.VSCode_profile.ps1
+++ winfiles/files/Microsoft.VSCode_profile.ps1
@@ -7,4 +7,4 @@ Configuration for the Microsoft.VSCode_profile.ps1 which loads the
 Microsoft.PowerShell_profile.ps1 file.
 #>
 
-. %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+. Join-Path -Path $env:USERPROFILE -ChildPath '\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
