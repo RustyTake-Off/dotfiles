15939519bc0883560ee90a92cd6939e844c93c6e
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-04-21 22:09:36 +0200

    Improved powershell setup script

--- setup.ps1
+++ setup.ps1
@@ -2,11 +2,14 @@
 # │ ╓──┐ ║════════╗  ╓─┐  ║
 # │ ╚══╛ ║──┐  ╓──╜  ║ │  ║  RustyTake-Off
 # │ ╓─┐ ╓╜  │  ║  │  ║ │  ║  https://github.com/RustyTake-Off
-# │ ║ │ ╚╗  │  ║  │  ╚═╛  ║
+# │ ║ │ ╚╗  │  ║  │  ╚═╛  ║  https://github.com/RustyTake-Off/dotfiles
 # └─╜ └──╜  └──╜  └───────╜
-# Setup script
+# setup script
 
 [CmdletBinding(SupportsShouldProcess)]
+
+# directories to checkout from repo
+$gitDirs = '.dots .gitconfig'
 
 # ANSI escape sequences for different colors
 $redColor = [char]27 + '[31m'
@@ -17,28 +20,36 @@ $purpleColor = [char]27 + '[35m'
 $resetColor = [char]27 + '[0m'
 
 function CheckAndAskToInstall([string]$packageName) {
+    <#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .PARAMETER packageName
+
+    #>
+
     if (-not (Get-Command -Name $packageName -ErrorAction SilentlyContinue)) {
         $packageNameCapitalized = $packageName.Substring(0, 1).ToUpper() + $packageName.Substring(1)
-        Write-Host "`n$redColor$packageNameCapitalized$resetColor is not installed."
-
+        Write-Host "$redColor$packageNameCapitalized$resetColor is not installed."
 
         $loop = $true
         while ($loop) {
             $choice = Read-Host "Do you want to install $yellowColor$packageNameCapitalized$resetColor (y/N)?"
 
             if ([string]::IsNullOrWhiteSpace($choice)) {
-                $choice = 'n' # Set default value to 'n' if no input is provided
+                $choice = 'n' # set default value to 'n' if no input is provided
             } else {
                 $choice = $choice.Trim().ToLower()
             }
 
             if ($choice -eq 'n') {
                 Write-Host 'Stopping script.'
-                return $false
+                exit 1
             } elseif ($choice -eq 'y') {
                 return $true
             } else {
-                Write-Host "$($redColor)Invalid input$($resetColor) please enter 'y' or 'n'.`n"
+                Write-Host "$($redColor)Invalid input$($resetColor) please enter 'y' or 'n'."
             }
         }
     }
@@ -47,55 +58,38 @@ function CheckAndAskToInstall([string]$packageName) {
 if (CheckAndAskToInstall 'winget') {
     Write-Host "Installing $($yellowColor)Winget$($resetColor) and its dependencies..."
 
-    $tempDir = "$env:USERPROFILE\tempDir"
-    if (-not ($null = Test-Path -Path $tempDir -PathType Container)) {
-        Write-Host 'Creating temp directory...'
-        $null = New-Item -Path $tempDir -ItemType Directory
-    }
-
-    # https://learn.microsoft.com/en-us/windows/package-manager/winget/
-    $components = @(
-        @{
-            name     = 'VCLibs'
-            fileName = 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-            url      = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-        },
-        @{
-            name     = 'Xaml'
-            fileName = 'Microsoft.UI.Xaml.2.8.x64.appx'
-            url      = 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx'
-        },
-        @{
-            name     = 'Winget'
-            fileName = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-            url      = 'https://aka.ms/getwinget'
-        }
-    )
-
-    for ($i = 0; $i -lt $components.Count; $i++) {
-        try {
-            Write-Host "Downloading $purpleColor$($components[$i].name)$resetColor..."
-            # Invoke-WebRequest -Uri $components[$i].url -OutFile "$tempDir\$($components[$i].fileName)"
-        } catch {
-            Write-Error "Failed to download $purpleColor$($components[$i].name)$redColor. Error: $_"
-            Write-Error "Line: $($_.ScriptStackTrace)"
-        }
-    }
-    for ($i = 0; $i -lt $components.Count; $i++) {
-        try {
-            Write-Host "Installing $purpleColor$($components[$i].fileName)$resetColor..."
-            # Add-AppxPackage -Path "$tempDir\$($components[$i].fileName)"
-        } catch {
-            Write-Error "Failed to install $purpleColor$($components[$i].fileName)$redColor. Error: $_"
-            Write-Error "Line: $($_.ScriptStackTrace)"
-        }
-    }
+    # https://github.com/ChrisTitusTech/winutil
+    # https://github.com/asheroto/winget-install
+    # (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/asheroto/winget-install/master/winget-install.ps1').Content | Invoke-Expression
 
     Write-Host "Installed $($greenColor)Winget$($resetColor) and its dependencies!"
+} elseif (Get-Command -Name winget -ErrorAction SilentlyContinue) {
+    Write-Host "$($greenColor)Winget$($resetColor) is installed."
 }
 
 if (CheckAndAskToInstall 'git') {
-    Write-Host "Installing $($purpleColor)Git$($resetColor)..."
-    Start-Process winget -ArgumentList 'install --exact --id Git.Git --source winget --interactive --accept-package-agreements --accept-source-agreements' -NoNewWindow -Wait
+    Write-Host "Installing $($yellowColor)Git$($resetColor)..."
+
+    # Start-Process winget -ArgumentList 'install --exact --id Git.Git --source winget --interactive --accept-package-agreements --accept-source-agreements' -NoNewWindow -Wait
+
     Write-Host "Installed $($greenColor)Git$($resetColor)!"
+} elseif (Get-Command -Name git -ErrorAction SilentlyContinue) {
+    Write-Host "$($greenColor)Git$($resetColor) is installed."
+}
+
+try {
+    if (Get-Command -Name 'git' -ErrorAction SilentlyContinue) {
+        git clone --bare 'https://github.com/RustyTake-Off/dotfiles.git' "$env:USERPROFILE\.dotfiles"
+
+        git --git-dir="$env:USERPROFILE\.dotfiles" --work-tree=$env:USERPROFILE checkout $gitDirs
+
+        git --git-dir="$env:USERPROFILE\.dotfiles" --work-tree=$env:USERPROFILE config status.showUntrackedFiles no
+    } else {
+        Write-Host "$($redColor)Git$($resetColor) is not installed!"
+        exit 1
+    }
+} catch {
+    Write-Error "Failed setting up dotfiles: $_"
+    Write-Error "Line: $($_.ScriptStackTrace)"
+    exit 1
 }
