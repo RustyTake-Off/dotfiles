da3ebc294fb7d4843d823ed132f03e84a4f04bfd
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-04-28 23:04:58 +0200

    Update VSCode profile

--- winfiles/.dots/powershell_profile/Microsoft.VSCode_profile.ps1
+++ winfiles/.dots/powershell_profile/Microsoft.VSCode_profile.ps1
@@ -1,9 +1,18 @@
-# ╒══════╗        ╒═══════╗
-# │ ╓──┐ ║════════╗  ╓─┐  ║
-# │ ╚══╛ ║──┐  ╓──╜  ║ │  ║  RustyTake-Off
-# │ ╓─┐ ╓╜  │  ║  │  ║ │  ║  https://github.com/RustyTake-Off
-# │ ║ │ ╚╗  │  ║  │  ╚═╛  ║
-# └─╜ └──╜  └──╜  └───────╜
-# PowerShell VSCode profile config.
+<#
+.SYNOPSIS
+Loads PowerShell profile script when a shell in VSCode is opened.
 
-Invoke-Expression (Join-Path -Path $env:USERPROFILE -ChildPath '\Documents\PowerShell\Microsoft.PowerShell_profile.ps1')
+.DESCRIPTION
+Loads PowerShell profile script when a shell in VSCode is opened.
+
+.LINK
+GitHub - https://github.com/RustyTake-Off/dotfiles
+#>
+
+$profilePath = Join-Path -Path $env:USERPROFILE -ChildPath '\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
+
+if (Test-Path $profilePath -PathType Leaf) {
+    Invoke-Expression $profilePath
+} else {
+    Write-Host "PowerShell profile path '$profilePath' does not exist. Please ensure the path is correct and the file exists."
+}
