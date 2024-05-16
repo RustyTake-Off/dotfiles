b2790ab2bb23e23b8ebad7f45a6bea8477e8b3f0
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-05-16 23:46:36 +0200

    Update code

--- winfiles/.dots/powershell_profile/Microsoft.PowerShell_profile.ps1
+++ winfiles/.dots/powershell_profile/Microsoft.PowerShell_profile.ps1
@@ -11,21 +11,19 @@ GitHub Repo - https://github.com/RustyTake-Off/dotfiles
 
 .NOTES
 Author  - RustyTake-Off
-Version - 0.1.1
+Version - 0.1.2
 #>
 
-try {
-    # Load powershell profile scripts
-    $profileScripts = @('functions', 'completions')
-    $profilePath = "$HOME/.dots/scripts"
+$profileScripts = @('functions', 'completions')
+$profilePath = "$HOME/.dots/scripts"
 
+try {
     foreach ($script in $profileScripts) {
         if (Test-Path -Path "$profilePath/$script" -PathType Leaf) {
             Invoke-Expression "$profilePath/$script"
         }
     }
 
-    # Initialize other tools
     Get-Command -Name starship && Invoke-Expression (&starship init powershell)
     Get-Command -Name zoxide && Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
--- winfiles/.dots/powershell_profile/Microsoft.VSCode_profile.ps1
+++ winfiles/.dots/powershell_profile/Microsoft.VSCode_profile.ps1
@@ -11,21 +11,16 @@ GitHub Repo - https://github.com/RustyTake-Off/dotfiles
 
 .NOTES
 Author  - RustyTake-Off
-Version - 0.1.1
+Version - 0.1.2
 #>
-
-$ErrorActionPreference = 'SilentlyContinue'
 
 $profilePath = "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
 
 try {
     if (Test-Path $profilePath -PathType Leaf) {
         Invoke-Expression $profilePath
-    } else {
-        throw "PowerShell profile in '$profilePath' does not exist. Ensure the path is correct and the file exists"
     }
 
-    $ErrorActionPreference = 'Continue'
     exit 0 # success
 } catch {
     "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
