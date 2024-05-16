4ca01ba08b85b563feb6364e986bd619d3691745
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-05-16 23:44:46 +0200

    Update code

--- winfiles/.dots/powershell_profile/Microsoft.VSCode_profile.ps1
+++ winfiles/.dots/powershell_profile/Microsoft.VSCode_profile.ps1
@@ -11,18 +11,21 @@ GitHub Repo - https://github.com/RustyTake-Off/dotfiles
 
 .NOTES
 Author  - RustyTake-Off
-Version - 0.1.0
+Version - 0.1.1
 #>
 
-try {
-    $profilePath = "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
+$ErrorActionPreference = 'SilentlyContinue'
 
+$profilePath = "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
+
+try {
     if (Test-Path $profilePath -PathType Leaf) {
         Invoke-Expression $profilePath
     } else {
-        throw "PowerShell profile in '$profilePath' does not exist. Please ensure the path is correct and the file exists."
+        throw "PowerShell profile in '$profilePath' does not exist. Ensure the path is correct and the file exists"
     }
 
+    $ErrorActionPreference = 'Continue'
     exit 0 # success
 } catch {
     "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
