e803ce42518980037c1bf1f1cd63648ce9fcf057
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-06-14 23:28:46 +0200

    Update linux set-dotfiles

--- wslfiles/.dots/scripts/set-dotfiles.sh
+++ wslfiles/.dots/scripts/set-dotfiles.sh
@@ -1,35 +1,93 @@
 #!/usr/bin/env bash
-# ╒══════╗        ╒═══════╗
-# │ ╓──┐ ║════════╗  ╓─┐  ║
-# │ ╚══╛ ║──┐  ╓──╜  ║ │  ║  RustyTake-Off
-# │ ╓─┐ ╓╜  │  ║  │  ║ │  ║  https://github.com/RustyTake-Off
-# │ ║ │ ╚╗  │  ║  │  ╚═╛  ║
-# └─╜ └──╜  └──╜  └───────╜
-# Script for setting up WSL dotfiles.
+# Dotfiles setup script
 
-# Repository      -   "https://github.com/RustyTake-Off/wsl-dotfiles",
-# Script file     -   "https://github.com/RustyTake-Off/wsl-dotfiles/blob/main/.local/bin/set-dotfiles.sh"
+# GitHub        - https://github.com/RustyTake-Off
+# GitHub Repo   - https://github.com/RustyTake-Off/dotfiles
+# Author        - RustyTake-Off
+# Version       - 0.1.0
 
-# ================================================================================
-# Main variables
+set -euo pipefail
 
-REPO_URL="https://github.com/RustyTake-Off/wsl-dotfiles.git"
+# Configuration variables
+repoUrl='https://github.com/RustyTake-Off/dotfiles.git'
+dotfilesPath="$HOME/.dotfiles"
+wslfilesPath="$HOME/wslfiles"
+declare -A toCheckout=(
+    ["wslfiles"]=(".config" ".dots" ".bash_logout" ".bash_profile" ".bashrc" ".gitconfig" ".hushlogin" ".inputrc")
+)
 
-# ================================================================================
-# Main code
+# ANSI escape sequences for different colors
+declare -A colors=(
+    ["red"]="\033[31m"
+    ["green"]="\033[32m"
+    ["yellow"]="\033[33m"
+    ["blue"]="\033[34m"
+    ["purple"]="\033[35m"
+    [reset]=$'\e[0m'
+)
 
-# Invokes dotfiles setup script
-if [ -d "$HOME/.dotfiles" ]; then
-  if [ -f "$(command -v git)" ]; then
-    git clone --bare "$REPO_URL" "$HOME/.dotfiles"
-    git --git-dir="$HOME\.dotfiles" --work-tree="$HOME" checkout
-    git --git-dir="$HOME\.dotfiles" --work-tree="$HOME" config status.showUntrackedFiles no
-  else
-    echo "Git is not installed!"
-  fi
+# Function definitions
+write_colored_message() {
+    local message=$1
+    local color=$2
+    echo -e "${colors[$color]}$message${colors[reset]}"
+}
+
+# Main logic
+# Clone dotfiles
+if [[ ! $skipDotfiles ]]; then
+    if ! command -v git >/dev/null; then
+        write_colored_message "Git is not installed" "red"
+        exit 1
+    fi
+
+    if [[ ! -d $dotfilesPath ]]; then
+        paths=()
+        for category in "${!toCheckout[@]}"; do
+            for item in ${toCheckout[$category]}; do
+                paths+=("$category/$item")
+            done
+        done
+
+        write_colored_message "Cloning dotfiles..." 'yellow'
+
+        git clone --bare "$repoUrl" "$dotfilesPath"
+        git --git-dir="$dotfilesPath" --work-tree="$HOME" checkout "${paths[@]}"
+        git --git-dir="$dotfilesPath" --work-tree="$HOME" config status.showUntrackedFiles no
+
+        if [[ -d $wslfilesPath ]]; then
+            for item in ${toCheckout[wslfiles]}; do
+                sourcePath="$wslfilesPath/$item"
+                if [[ -d $sourcePath ]]; then
+                    find "$sourcePath" -mindepth 1 -maxdepth 1 -exec mv -t "$HOME" {} +
+                elif [[ -f $sourcePath ]]; then
+                    mv "$sourcePath" "$HOME"
+                fi
+            done
+        else
+            write_colored_message "Directory 'wslfiles' not found in '$HOME'" "red"
+            exit 1
+        fi
+    else
+        write_colored_message "Dotfiles are set. Checking for updates..." 'yellow'
+
+        git --git-dir="$dotfilesPath" --work-tree="$HOME" reset --hard
+        git --git-dir="$dotfilesPath" --work-tree="$HOME" pull
+
+        if [[ -d $wslfilesPath ]]; then
+            for item in ${toCheckout[wslfiles]}; do
+                sourcePath="$wslfilesPath/$item"
+                if [[ -d $sourcePath ]]; then
+                    find "$sourcePath" -mindepth 1 -maxdepth 1 -exec mv -t "$HOME" {} +
+                elif [[ -f $sourcePath ]]; then
+                    mv "$sourcePath" "$HOME"
+                fi
+            done
+        else
+            write_colored_message "Directory 'wslfiles' not found in '$HOME'" "red"
+            exit 1
+        fi
+    fi
 else
-  echo "Dotfiles are set"
-  echo "Checking for updates"
-  git --git-dir="$HOME\.dotfiles" --work-tree="$HOME" reset --hard
-  git --git-dir="$HOME\.dotfiles" --work-tree="$HOME" pull
+    write_colored_message 'Skipping dotfiles' 'yellow'
 fi
