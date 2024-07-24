1c0360e5f79378601b5c0c04b0dc9c33c473c308
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-07-24 21:22:09 +0200

    Update setdots, wslup

--- wslfiles/.dots/scripts/set_dotfiles.sh
+++ wslfiles/.dots/scripts/set_dotfiles.sh
@@ -4,7 +4,7 @@
 # GitHub        - https://github.com/RustyTake-Off
 # GitHub Repo   - https://github.com/RustyTake-Off/dotfiles
 # Author        - RustyTake-Off
-# Version       - 0.1.3
+# Version       - 0.1.4
 
 # Configuration variables
 readonly REPO_URL="https://github.com/RustyTake-Off/dotfiles.git"
@@ -31,22 +31,27 @@ write_colored_message() {
 }
 
 # Main logic
-# Clone dotfiles
-if [ ! -x "$(command -v git)" ]; then
+# Check if Git is installed
+if ! command -v git &> /dev/null; then
   write_colored_message "Git is not installed" "red"
   exit 1
 fi
 
-if [ ! -d $DOTFILES_PATH ]; then
+# Clone dotfiles
+if [ ! -d "$DOTFILES_PATH" ]; then
   write_colored_message "Cloning dotfiles..." "yellow"
 
-  git clone --bare "$REPO_URL" "$DOTFILES_PATH"
-  git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" checkout $WSLFILES_PATH
-  git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" config status.showUntrackedFiles no
+  if git clone --bare "$REPO_URL" "$DOTFILES_PATH"; then
+    git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" checkout "$WSLFILES_PATH"
+    git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" config status.showUntrackedFiles no
+  else
+    write_colored_message "Failed to clone dotfiles repository" "red"
+    exit 1
+  fi
 else
   write_colored_message "Dotfiles are set" "yellow"
   write_colored_message "Checking for updates..." "purple"
 
   git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" reset --hard
-  git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" pull origin $WSLFILES_PATH
+  git --git-dir="$DOTFILES_PATH" --work-tree="$HOME" pull origin "$WSLFILES_PATH"
 fi
