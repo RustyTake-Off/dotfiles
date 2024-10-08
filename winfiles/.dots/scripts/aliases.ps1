08114deeabd2311f888bf094adeb9e81e9c78863
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-10-08 03:42:12 +0200

    Misc update

--- winfiles/.dots/scripts/aliases.ps1
+++ winfiles/.dots/scripts/aliases.ps1
@@ -11,132 +11,131 @@ GitHub Repo - https://github.com/RustyTake-Off/dotfiles
 
 .NOTES
 Author  - RustyTake-Off
-Version - 0.1.12
 #>
 
 [CmdletBinding(SupportsShouldProcess)]
 param()
 
-begin {
-    # Preferences
-    $ErrorActionPreference = 'SilentlyContinue'
-}
+# Preferences
+$ErrAction = $ErrorActionPreference
+$ErrorActionPreference = 'SilentlyContinue'
 
-process {
-    try {
-        # Common functions
-        function cd...... { Set-Location ../../../../../.. }
-        function cd..... { Set-Location ../../../../.. }
-        function cd.... { Set-Location ../../../.. }
-        function cd... { Set-Location ../../.. }
-        function cd.. { Set-Location ../.. }
-        function cd. { Set-Location .. }
-        function hm { Set-Location $HOME }
-        function hpr { Set-Location "$HOME/pr" }
-        function hwk { Set-Location "$HOME/wk" }
-        function dl { Set-Location "$HOME/Downloads" }
-        function desk { Set-Location "$HOME/Desktop" }
-        function docs { Set-Location "$HOME/Documents" }
+# Main execution logic
+try {
+    # Common functions
+    function cd...... { Set-Location ../../../../../.. }
+    function cd..... { Set-Location ../../../../.. }
+    function cd.... { Set-Location ../../../.. }
+    function cd... { Set-Location ../../.. }
+    function cd.. { Set-Location ../.. }
+    function cd. { Set-Location .. }
+    function hm { Set-Location $HOME }
+    function hpr { Set-Location "$HOME/pr" }
+    function hwk { Set-Location "$HOME/wk" }
+    function dl { Set-Location "$HOME/Downloads" }
+    function desk { Set-Location "$HOME/Desktop" }
+    function docs { Set-Location "$HOME/Documents" }
 
-        function h { Get-History }
-        function cls { Clear-Host }
+    function h { Get-History }
+    function cls { Clear-Host }
 
-        function ls { Get-ChildItem }
-        function la { Get-ChildItem }
-        function ll { Get-ChildItem }
+    function ls { Get-ChildItem }
+    function la { Get-ChildItem }
+    function ll { Get-ChildItem }
 
-        function df {
-            Get-Volume
-        }
-
-        # Create new env variable
-        function export ([string]$Name, [string]$Value) {
-            Set-Item -Path "env:$Name" -Value $Value -Force
-        }
-
-        function pkill ([string]$Name) {
-            Get-Process -Name $Name -ErrorAction SilentlyContinue | Stop-Process
-        }
-
-        function pgrep ([string]$Name) {
-            Get-Process | Where-Object { $_.ProcessName -like "*$Name*" }
-        }
-
-        function head ([string]$Path, [int]$N = 10) {
-            Get-Content -Path $Path -Head $N
-        }
-
-        function tail ([string]$Path, [int]$N = 10) {
-            Get-Content -Path $Path -Tail $N
-        }
-
-        function touch ([string]$File) {
-            Write-Output '' | Out-File -FilePath $File -Encoding ASCII
-        }
-
-        # Find files
-        function ff ([string]$Name) {
-            Get-ChildItem -Recurse -Filter "*$Name*" -ErrorAction SilentlyContinue | ForEach-Object {
-                Write-Output $_.FullName
-            }
-        }
-
-        # Find commands
-        function which {
-            Get-Command -Name $Args | Select-Object -ExpandProperty Definition
-        }
-
-        function md5 { Get-FileHash -Algorithm MD5 $Args }
-        function sha1 { Get-FileHash -Algorithm SHA1 $Args }
-        function sha256 { Get-FileHash -Algorithm SHA256 $Args }
-
-        # Get public ips
-        function pubip4 { (Invoke-WebRequest -Uri 'https://api.ipify.org/').Content }
-        function pubip6 { (Invoke-WebRequest -Uri 'https://ifconfig.me/ip').Content }
-
-        function sysinfo { Get-ComputerInfo }
-        function flushdns { Clear-DnsClientCache }
-
-        # Open windows explorer
-        function open ([string]$Path) {
-            explorer.exe $Path
-        }
-
-        # Quick admin
-        function admin {
-            if ($Args) {
-                Start-Process wt -Verb RunAs -ArgumentList "pwsh -NoExit -NoLogo -ExecutionPolicy Bypass -WorkingDirectory $(Get-Location) -Command $Args"
-            } else {
-                Start-Process wt -Verb RunAs -ArgumentList "pwsh -NoExit -NoLogo -ExecutionPolicy Bypass -WorkingDirectory $(Get-Location)"
-            }
-        }
-
-        # Manage powershell profile
-        function editpsp {
-            notepad $PROFILE
-        }
-
-        function repsp {
-            Invoke-Expression $PROFILE
-        }
-
-        function revcp {
-            Invoke-Expression ($PROFILE -replace 'PowerShell_profile', 'VSCode_profile')
-        }
-
-        # Manage dotfiles in $HOME directory
-        function dot {
-            git --git-dir="$HOME/.dotfiles" --work-tree=$HOME $Args
-        }
-
-        function setdots {
-            Invoke-Expression "$HOME/.dots/scripts/set-dotfiles.ps1"
-        }
-
-        function winup {
-            Invoke-Expression "$HOME/.dots/scripts/winup.ps1 $Args"
-        }
-    } catch {
-        throw "Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+    function df {
+        Get-Volume
     }
+
+    # Create new env variable
+    function export ([string]$Name, [string]$Value) {
+        Set-Item -Path "env:$Name" -Value $Value -Force
+    }
+
+    function pkill ([string]$Name) {
+        Get-Process -Name $Name -ErrorAction SilentlyContinue | Stop-Process
+    }
+
+    function pgrep ([string]$Name) {
+        Get-Process | Where-Object { $_.ProcessName -like "*$Name*" }
+    }
+
+    function head ([string]$Path, [int]$N = 10) {
+        Get-Content -Path $Path -Head $N
+    }
+
+    function tail ([string]$Path, [int]$N = 10) {
+        Get-Content -Path $Path -Tail $N
+    }
+
+    function touch ([string]$File) {
+        Write-Output '' | Out-File -FilePath $File -Encoding ASCII
+    }
+
+    # Find files
+    function ff ([string]$Name) {
+        Get-ChildItem -Recurse -Filter "*$Name*" -ErrorAction SilentlyContinue | ForEach-Object {
+            Write-Output $_.FullName
+        }
+    }
+
+    # Find commands
+    function which {
+        Get-Command -Name $Args | Select-Object -ExpandProperty Definition
+    }
+
+    function md5 { Get-FileHash -Algorithm MD5 $Args }
+    function sha1 { Get-FileHash -Algorithm SHA1 $Args }
+    function sha256 { Get-FileHash -Algorithm SHA256 $Args }
+
+    # Get public ips
+    function pubip4 { (Invoke-WebRequest -Uri 'https://api.ipify.org/').Content }
+    function pubip6 { (Invoke-WebRequest -Uri 'https://ifconfig.me/ip').Content }
+
+    function sysinfo { Get-ComputerInfo }
+    function flushdns { Clear-DnsClientCache }
+
+    # Open windows explorer
+    function open ([string]$Path) {
+        explorer.exe $Path
+    }
+
+    # Quick admin
+    function admin {
+        if ($Args) {
+            Start-Process wt -Verb RunAs -ArgumentList "pwsh -NoExit -NoLogo -ExecutionPolicy Bypass -WorkingDirectory $(Get-Location) -Command $Args"
+        } else {
+            Start-Process wt -Verb RunAs -ArgumentList "pwsh -NoExit -NoLogo -ExecutionPolicy Bypass -WorkingDirectory $(Get-Location)"
+        }
+    }
+
+    # Manage powershell profile
+    function editpsp {
+        notepad $PROFILE
+    }
+
+    function repsp {
+        Invoke-Expression $PROFILE
+    }
+
+    function revcp {
+        Invoke-Expression ($PROFILE -replace 'PowerShell_profile', 'VSCode_profile')
+    }
+
+    # Manage dotfiles in $HOME directory
+    function dot {
+        git --git-dir="$HOME/.dotfiles" --work-tree=$HOME $Args
+    }
+
+    function setdots {
+        Invoke-Expression "$HOME/.dots/scripts/set-dotfiles.ps1"
+    }
+
+    function winup {
+        Invoke-Expression "$HOME/.dots/scripts/winup.ps1 $Args"
+    }
+} catch {
+    throw "Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+} finally {
+    $ErrorActionPreference = $ErrAction
 }
--- winfiles/.dots/scripts/completions.ps1
+++ winfiles/.dots/scripts/completions.ps1
@@ -11,85 +11,84 @@ GitHub Repo - https://github.com/RustyTake-Off/dotfiles
 
 .NOTES
 Author  - RustyTake-Off
-Version - 0.1.8
 #>
 
 [CmdletBinding(SupportsShouldProcess)]
 param()
 
-begin {
-    # Preferences
-    $ErrorActionPreference = 'SilentlyContinue'
-}
+# Preferences
+$ErrAction = $ErrorActionPreference
+$ErrorActionPreference = 'SilentlyContinue'
 
-process {
-    try {
-        # Completion and history for powershell
-        if (Get-Module -Name PSReadLine) {
-            $script:psMinimumVersion = [version]'7.1.999'
+# Main execution logic
+try {
+    # Completion and history for powershell
+    if (Get-Module -Name PSReadLine) {
+        $script:psMinimumVersion = [version]'7.1.999'
 
-            if (($Host.Name -eq 'ConsoleHost') -and ($PSVersionTable.PSVersion -ge $script:psMinimumVersion)) {
-                Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-            } else {
-                Set-PSReadLineOption -PredictionSource History
-            }
-            # Set-PSReadLineOption -EditMode Vi
-            Set-PSReadLineOption -HistoryNoDuplicates:$true
-            Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
-            Set-PSReadLineOption -PredictionViewStyle ListView
-            Set-PSReadLineOption -Colors @{
-                Command   = 'Yellow'
-                Parameter = 'Green'
-                String    = 'DarkCyan'
-            }
-            Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-            Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+        if (($Host.Name -eq 'ConsoleHost') -and ($PSVersionTable.PSVersion -ge $script:psMinimumVersion)) {
+            Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+        } else {
+            Set-PSReadLineOption -PredictionSource History
         }
-
-        # Completion for git
-        if (-not (Get-Module -Name posh-git)) {
-            Import-Module -Name posh-git
+        # Set-PSReadLineOption -EditMode Vi
+        Set-PSReadLineOption -HistoryNoDuplicates:$true
+        Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
+        Set-PSReadLineOption -PredictionViewStyle ListView
+        Set-PSReadLineOption -Colors @{
+            Command   = 'Yellow'
+            Parameter = 'Green'
+            String    = 'DarkCyan'
         }
-
-        if ($GitPromptSettings) {
-            $GitPromptSettings.EnablePromptStatus = $false
-            $GitPromptSettings.EnableFileStatus = $false
-        }
-
-        # Completion for winget - https://learn.microsoft.com/en-us/windows/package-manager/winget/tab-completion#enable-tab-completion
-        # if (Get-Command -Name winget) {
-        #     Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-        #         param($wordToComplete, $commandAst, $cursorPosition)
-        #         [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        #         $Local:word = $wordToComplete.Replace('"', '""')
-        #         $Local:ast = $commandAst.ToString().Replace('"', '""')
-        #         winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-        #             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        #         }
-        #     }
-        # }
-
-        # Completion for azure-cli - https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?view=azure-cli-latest&tabs=winget#enable-tab-completion-in-powershell
-        # if (Get-Command -Name az) {
-        #     Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
-        #         param($commandName, $wordToComplete, $cursorPosition)
-        #         $completion_file = New-TemporaryFile
-        #         $env:ARGCOMPLETE_USE_TEMPFILES = 1
-        #         $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
-        #         $env:COMP_LINE = $wordToComplete
-        #         $env:COMP_POINT = $cursorPosition
-        #         $env:_ARGCOMPLETE = 1
-        #         $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
-        #         $env:_ARGCOMPLETE_IFS = "`n"
-        #         $env:_ARGCOMPLETE_SHELL = 'powershell'
-        #         az 2>&1 | Out-Null
-        #         Get-Content $completion_file | Sort-Object | ForEach-Object {
-        #             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        #         }
-        #         Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
-        #     }
-        # }
-    } catch {
-        throw "Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+        Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+        Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
     }
+
+    # Completion for git
+    if (-not (Get-Module -Name posh-git)) {
+        Import-Module -Name posh-git
+    }
+
+    if ($GitPromptSettings) {
+        $GitPromptSettings.EnablePromptStatus = $false
+        $GitPromptSettings.EnableFileStatus = $false
+    }
+
+    # Completion for winget - https://learn.microsoft.com/en-us/windows/package-manager/winget/tab-completion#enable-tab-completion
+    # if (Get-Command -Name winget) {
+    #     Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    #         param($wordToComplete, $commandAst, $cursorPosition)
+    #         [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    #         $Local:word = $wordToComplete.Replace('"', '""')
+    #         $Local:ast = $commandAst.ToString().Replace('"', '""')
+    #         winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+    #             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    #         }
+    #     }
+    # }
+
+    # Completion for azure-cli - https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?view=azure-cli-latest&tabs=winget#enable-tab-completion-in-powershell
+    # if (Get-Command -Name az) {
+    #     Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+    #         param($commandName, $wordToComplete, $cursorPosition)
+    #         $completion_file = New-TemporaryFile
+    #         $env:ARGCOMPLETE_USE_TEMPFILES = 1
+    #         $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+    #         $env:COMP_LINE = $wordToComplete
+    #         $env:COMP_POINT = $cursorPosition
+    #         $env:_ARGCOMPLETE = 1
+    #         $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+    #         $env:_ARGCOMPLETE_IFS = "`n"
+    #         $env:_ARGCOMPLETE_SHELL = 'powershell'
+    #         az 2>&1 | Out-Null
+    #         Get-Content $completion_file | Sort-Object | ForEach-Object {
+    #             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    #         }
+    #         Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+    #     }
+    # }
+} catch {
+    throw "Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+} finally {
+    $ErrorActionPreference = $ErrAction
 }
